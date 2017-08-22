19c6f75da115326c3b3df10bd1b710cd4406373d
黄家成(997332) <997332>
2017-08-22 21:18:05 +0800

    商品 图片添加

---
+++ src/main/java/com/cn/website/mcxs/bean/GoodsImages.java
@@ -0,0 +1,98 @@
+package com.cn.website.mcxs.bean;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "goods_images")
+public class GoodsImages implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name ="img_link")
+	private String link;
+	
+	@Column(name ="img_name")
+	private String name;
+	
+	@Column(name ="img_size")
+	private float size;
+	
+	@Column(name ="img_addtime")
+	private Date addtime;
+	
+	@Column(name ="img_ext")
+	private String ext;
+	
+	@Column(name ="goodsid")
+	private String goodsid;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getSize() {
+		return size;
+	}
+
+	public void setSize(float size) {
+		this.size = size;
+	}
+
+	public Date getAddtime() {
+		return addtime;
+	}
+
+	public void setAddtime(Date addtime) {
+		this.addtime = addtime;
+	}
+
+	public String getExt() {
+		return ext;
+	}
+
+	public void setExt(String ext) {
+		this.ext = ext;
+	}
+
+	public String getGoodsid() {
+		return goodsid;
+	}
+
+	public void setGoodsid(String goodsid) {
+		this.goodsid = goodsid;
+	}
+	
+	
+}
